473078e0b0b92d6dda9781bc2dfaef1ba7bf9279
Zach Wells <zwucsc@gmail.com>
2021-09-06 10:00:03 -0700

    basic data flow for chip video

---
+++ src/TitleScreen.py
@@ -0,0 +1,15 @@
+import configparser
+
+import sys
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+def TerminalScreen(argv):
+
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(argv)
+
+    print("FFmpeg utility: ")
+
+
